b80b002d716e36c2392282fa1289398ff24cdd15
Noelie JESTIN <noelie_jestin@outlook.com>
2025-08-24 21:00:42 +0200

    Add about section in its own page

--- src/navigation.ts
+++ src/navigation.ts
@@ -2,7 +2,7 @@ export const headerData = {
   links: [
     { text: 'Home', href: '/#home' },
     { text: 'Services', href: '/#services' },
-    { text: 'About', href: '/#about' },
+    { text: 'About', href: '/about' },
   ],
   actions: [{ text: 'Contact', href: '/contact', variant: 'primary' }],
 };
