eaf2166dfa21686372641f28dc7c3b525d56dd8e
Noelie JESTIN <noelie_jestin@outlook.com>
2025-08-24 21:47:33 +0200

    Better loading : Optimization of image size

--- src/components/widgets/Carousel.jsx
+++ src/components/widgets/Carousel.jsx
@@ -6,11 +6,11 @@ export default function DemoCarousel() {
   return (
     <Carousel>
       <div>
-        <img src="/drone.svg" alt="Slide 1" />
+        <img src="/IMG_1_Carrousel_Drone.webp" alt="Slide 1" />
         <p className="legend">Multitask system : RTOS, Linux software development</p>
       </div>
       <div>
-        <img src="/hero_page.svg" alt="Slide 3" />
+        <img src="/IMG_2_Carrousel_Agro.webp" alt="Slide 3" />
         <p className="legend">Prototype : PCB Design, sensors local or cloud display</p>
       </div>
     </Carousel>
