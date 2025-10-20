0ce256d91bb7c0b318ecf141133458a2092f997e
Noelie JESTIN <noelie_jestin@outlook.com>
2025-10-20 15:27:48 +0200

    About: remove about from naviguation

--- src/navigation.ts
+++ src/navigation.ts
@@ -2,7 +2,6 @@ export const headerData = {
   links: [
     { text: 'Home', href: '/#home' },
     { text: 'Services', href: '/#services' },
-    { text: 'About', href: '/about/' },
   ],
   actions: [{ text: 'Contact', href: '/contact/', variant: 'primary' }],
 };
