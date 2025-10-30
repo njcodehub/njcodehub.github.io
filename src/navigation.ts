fdf3320d90e1b4365d2f627166c0d7cd72993061
Noelie JESTIN <noelie_jestin@outlook.com>
2025-10-30 01:34:36 +0100

    Services: details cards

--- src/navigation.ts
+++ src/navigation.ts
@@ -2,6 +2,9 @@ export const headerData = {
   links: [
     { text: 'Home', href: '/#home' },
     { text: 'Sectors', href: '/#sectors' },
+    { text: 'Services', href: '/#services' },
+    { text: 'Method', href: '/#timeline' },
+    { text: 'About', href: '/#about-preview' },
   ],
   actions: [{ text: 'Contact', href: '/contact/', variant: 'primary' }],
 };
