ad94210f5c7cf24ad9d1641c78c0125c689ca4f0
Noelie JESTIN <noelie_jestin@outlook.com>
2025-10-30 02:05:33 +0100

    Better wording

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,7 +3,7 @@ export const headerData = {
     { text: 'Home', href: '/#home' },
     { text: 'Sectors', href: '/#sectors' },
     { text: 'Services', href: '/#services' },
-    { text: 'Method', href: '/#timeline' },
+    { text: 'How', href: '/#timeline' },
     { text: 'About', href: '/#about-preview' },
   ],
   actions: [{ text: 'Contact', href: '/contact/', variant: 'primary' }],
